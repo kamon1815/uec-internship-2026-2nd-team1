8115b34f0bc7f9eafebabcc41633e4f63067434e
isl-kojima <85148375+isl-kojima@users.noreply.github.com>
2026-09-03 10:59:54 +0900

    競合のための変更その２

--- web-camera-sample/hello-opencv_ss.py
+++ web-camera-sample/hello-opencv_ss.py
@@ -11,7 +11,7 @@ cv2.putText(img,
             "kk_test",
             org=(200, 50),
             fontFace=cv2.FONT_HERSHEY_DUPLEX,
-            fontScale=1.5,
+            fontScale=1,
             color=(0, 255, 0),
             thickness=2,
             lineType=cv2.LINE_AA)
