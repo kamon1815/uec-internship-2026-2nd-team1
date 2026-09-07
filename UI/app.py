d490337cd246d70ba6d98c9b3038f014d3c375fe
UEC1 <pj_internship_UEC1@photron.co.jp>
2026-09-07 11:46:40 +0900

    appのサンプルを反映

--- UI/app.py
+++ UI/app.py
@@ -1,34 +1,34 @@
-import os
+import bottle
+import ngrok
+import pathlib
+import threading
 
-def write1(file1, str1):
-    with open(file1, 'w', encoding='utf-8') as f1:
-        f1.write(str1)
-    return 0
+BASE_DIR = pathlib.Path(__file__).resolve().parent
+bottle.TEMPLATE_PATH.append(BASE_DIR / 'views')
+
+HOST = 'localhost'
+PORT = 8080
+PUBLIC_URL = 'cleaver-fraction-art.ngrok-free.dev'  # https://cleaver-fraction-art.ngrok-free.dev
 
 
-str1 = '''<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{title1}</title>
-</head>
-<body style="padding: 150px;">
-  {body1}
-  <video id="myVideo" controls>
-    <source src="mask.mp4" type="video/mp4" />
-  </video>
-</body>
+@bottle.get('/')
+def index():
+    return bottle.template('index')
 
-<style>
-#myVideo {{
-  width: 640px;
-  aspect-ratio: 5 / 4;
-}}
-</style>
-</html>'''.format(title1="hello html", body1="hello html!")
 
-print(str1)
+def run_server():
+    bottle.run(host=HOST, port=PORT)
 
-path1 = os.path.dirname(os.path.abspath(__file__)) + "/"
-file1 = path1 + "html1.html"
-write1(file1, str1)
+def main():
+    ngrok.forward(f'{HOST}:{PORT}', authtoken_from_env=True, domain=PUBLIC_URL)
+    threading.Thread(target=run_server, daemon=True).start()
+
+    try:
+        while True:
+            pass
+
+    except KeyboardInterrupt:
+        pass
+    
+if __name__ == '__main__':
+    main()
