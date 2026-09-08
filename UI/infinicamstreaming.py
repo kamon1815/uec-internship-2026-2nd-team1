a53c78b7cd2e171ad775179577136d8aff45a949
UEC1 <pj_internship_UEC1@photron.co.jp>
2026-09-08 11:14:27 +0900

    Update infinicamstreaming.py

--- UI/infinicamstreaming.py
+++ UI/infinicamstreaming.py
@@ -76,6 +76,8 @@ decoder = cam.decoder()
 reso = cam.resolution()
 
 GPUStatus = decoder.getAvailableGPUProcess()
+
+
 if GPUStatus:
     param = pypuclib.GPUSetup(reso.width, reso.height)
     decoder.setupGPUDecode(param)
@@ -98,6 +100,21 @@ def xfer_callback(xferData):
         with frame_lock:
             latest_jpg= encoded_image.tobytes()
 
+def get_mjpeg_stream():
+    while not shutdown_event.is_set():
+        with frame_lock:
+            frame = latest_jpeg
+
+        if frame is not None:
+            header = (
+                f'--frame\r\n'
+                f'Content-Type: image/jpeg\r\n'
+                f'Content-Length: {len(frame)}\r\n\r\n'
+            ).encode()
+            yield header + frame + b'\r\n'
+
+        time.sleep(0.03)
+
 
 
     if is_recording and ffmpeg_process is None:
@@ -107,44 +124,45 @@ def xfer_callback(xferData):
         stop_requested = False
         f_count = 0
 
+
         ffmpeg_process = (
-        ffmpeg 
-        .input(
-            "pipe:",
-            format="rawvideo",
-            pix_fmt="bgr24", 
-            s=f"{width}*{height}",
-            r=fps
-        )
-        .output(
-            str(path),
-            vcodec=vcodec,
-            pix_fmt="yuv420p",
-            r=fps
-        )
-        .overwrite_output()
-        .run_async(pipe_stdin=True)
-    )
-    print("録画開始")
+            ffmpeg 
+            .input(
+                "pipe:",
+                format="rawvideo",
+                pix_fmt="bgr24", 
+                s=f"{width}*{height}",
+                r=fps
+            )
+            .output(
+                str(path),
+                vcodec=vcodec,
+                pix_fmt="yuv420p",
+                r=fps
+            )
+            .overwrite_output()
+            .run_async(pipe_stdin=True)
+        )   
+        print("録画開始")
 
-if is_recording and ffmpeg_process is not None:
-    ffmpeg_process.stdin.write(save_frame.tobytes())
-    f_count += 1
+    if is_recording and ffmpeg_process is not None:
+        ffmpeg_process.stdin.write(save_frame.tobytes())
+        f_count += 1
 
-if is_recording and (
-    stop_requested or 
-    f_count >= MAX_SAVE_FRAME_COUNT
-):
-    ffmpeg_process.stdin.close()
-    ffmpeg_process.wait()
-    print("録画終了")
-    print("保存先：", path)
+    if is_recording and (
+        stop_requested or 
+        f_count >= MAX_SAVE_FRAME_COUNT
+    ):
+        ffmpeg_process.stdin.close()
+        ffmpeg_process.wait()
+        print("録画終了")
+        print("保存先：", path)
 
-    ffmpeg_process = None
-    is_recording = False
-    stop_requested = False
-    f_count = 0
-time.sleep(0.03)
+        ffmpeg_process = None
+        is_recording = False
+        stop_requested = False
+        f_count = 0
+    time.sleep(0.03)
 
 
 
