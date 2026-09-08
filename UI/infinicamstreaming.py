40e34f39a53db9c34b72dd03e222fa3f7bc41ff2
UEC03 <pj_internship_UEC3@photron.co.jp>
2026-09-08 14:54:32 +0900

    コード見やすくした

--- UI/infinicamstreaming.py
+++ UI/infinicamstreaming.py
@@ -18,21 +18,8 @@ frame_lock = threading.Lock()
 latest_jpeg = None
 shutdown_event = threading.Event()
 
-path = BASE_DIR / "recorded_movie.mp4"
-vcodec = "h264"
-MAX_SAVE_FRAME_COUNT = 10000
-fps = 30
-ffmpeg_process = None
-is_recording = False
-stop_requested = False
-f_count = 0
-start_time = 0.0
-
-import random
-
 def live_stream_loop():
     while not shutdown_event.is_set():
-        print(random.randint(1,6))
         with frame_lock:
             frame = latest_jpeg
         if frame is not None:
@@ -43,56 +30,6 @@ def live_stream_loop():
             ).encode()
             yield header + frame + b'\r\n'
         time.sleep(0.03)
-
-
-
-    if is_recording and ffmpeg_process is None:
-        height, width = frame.shape[:2]
-
-        start_time = time.time()
-        stop_requested = False
-        f_count = 0
-
-
-        ffmpeg_process = (
-            ffmpeg 
-            .input(
-                "pipe:",
-                format="rawvideo",
-                pix_fmt="bgr24", 
-                s=f"{width}*{height}",
-                r=fps
-            )
-            .output(
-                str(path),
-                vcodec=vcodec,
-                pix_fmt="yuv420p",
-                r=fps
-            )
-            .overwrite_output()
-            .run_async(pipe_stdin=True)
-        )   
-        print("録画開始")
-
-    if is_recording and ffmpeg_process is not None:
-        ffmpeg_process.stdin.write(save_frame.tobytes())
-        f_count += 1
-
-    if is_recording and (
-        stop_requested or 
-        f_count >= MAX_SAVE_FRAME_COUNT
-    ):
-        ffmpeg_process.stdin.close()
-        ffmpeg_process.wait()
-        print("録画終了")
-        print("保存先：", path)
-
-        ffmpeg_process = None
-        is_recording = False
-        stop_requested = False
-        f_count = 0
-    time.sleep(0.03)
-
 
 
 @bottle.get('/')
@@ -107,38 +44,25 @@ def live_stream():
     bottle.response.set_header('Expires', '0')
     return live_stream_loop()
 
+is_recording = False
+
 @bottle.post("/toggle_recording")
 def toggle_recoding():
     global is_recording
-    global stop_requested
     print("ボタンが押されました")
-    if not is_recording:
-        is_recording = True
-        stop_requested = False
-        return{
+    is_recording = not is_recording
+    if is_recording:
+        return {
             "recording":True
         }
     else:
-        stop_requested = True
-        return{
+        return {
             "recording":False
         }
 
-
-cam = pypuclib.CameraFactory().create()
-decoder = cam.decoder()
-reso = cam.resolution()
-
-GPUStatus = decoder.getAvailableGPUProcess()
-
-
-if GPUStatus:
-    param = pypuclib.GPUSetup(reso.width, reso.height)
-    decoder.setupGPUDecode(param)
-    print('Decode using a GPU device')
-else:
-    print('Since GPU is not available, decode using CPU')
-
+decoder = None
+reso = None
+GPUStatus = None
 
 def xfer_callback(xferData):
     global latest_jpeg
@@ -155,19 +79,29 @@ def xfer_callback(xferData):
             latest_jpeg = encoded_image.tobytes()
 
 
-
 def run_server():
     bottle.run(host=HOST, port=PORT, server='waitress')
 
+
 def main():
-    global is_recording
-    global stop_requested
-    global ffmpeg_process
-    global f_count
-    global start_time
+    global decoder, reso, GPUStatus
 
     #ngrok.forward(f'{HOST}:{PORT}', authtoken_from_env=True, domain=PUBLIC_URL)
     threading.Thread(target=run_server, daemon=True).start()
+
+
+    cam = pypuclib.CameraFactory().create()
+    decoder = cam.decoder()
+    reso = cam.resolution()
+    GPUStatus = decoder.getAvailableGPUProcess()
+    if GPUStatus:
+        param = pypuclib.GPUSetup(reso.width, reso.height)
+        decoder.setupGPUDecode(param)
+        print('Decode using a GPU device')
+    else:
+        print('Since GPU is not available, decode using CPU')
+
+
     try:
         cam.beginXfer(xfer_callback)
         print("INFINICAM開始")
@@ -178,11 +112,9 @@ def main():
         pass
 
     finally:
-        shutdown_event.set()
+        print("プログラムを終了します...")
 
-        if ffmpeg_process is not None:
-            ffmpeg_process.stdin.close()
-            ffmpeg_process.wait()
+        shutdown_event.set()
         cam.endXfer()
 
         if GPUStatus:
@@ -191,9 +123,7 @@ def main():
         # decoder.teardownGPUDecode()
         print("終了しました")
 
+
 if __name__ == '__main__':
     main()
-    
-    
-
     
