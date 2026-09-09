2038ff1b475649161c01f9f19c73511b39889d81
UEC5 <pj_internship_UEC5@photron.co.jp>
2026-09-09 16:27:58 +0900

    撮影、解析、保存を一貫してできるコードの作成

--- faster_capture/faster_capture_compressed.py
+++ faster_capture/faster_capture_compressed.py
@@ -222,7 +222,7 @@ def main():
     #    print(f'usage: {__file__} <output_file>')
     #    exit(1)
     
-    OUTPUT_FILE =  './faster_capture/output/coin3.npy'
+    OUTPUT_FILE =  './faster_capture/output/coin.npy'
 
     try:
     #    capture_infinicam(sys.argv[1])
