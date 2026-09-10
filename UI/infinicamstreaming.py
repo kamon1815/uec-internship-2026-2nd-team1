6c001a58dbf52bdd8120dd153ed377e13e9fd28a
UEC1 <pj_internship_UEC1@photron.co.jp>
2026-09-10 12:05:18 +0900

    mp4-display

--- UI/infinicamstreaming.py
+++ UI/infinicamstreaming.py
@@ -6,6 +6,19 @@ import threading
 import time
 import ffmpeg
 import pypuclib
+import tempfile
+import os
+import sys
+import shutil
+
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from faster_capture import npy_viewer
+
+from faster_capture.npy_saver import NpySaver
+# from tracking import tracking_UImerge
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 bottle.TEMPLATE_PATH.append(BASE_DIR / 'views')
@@ -17,17 +30,6 @@ PUBLIC_URL = 'cleaver-fraction-art.ngrok-free.dev'  # https://cleaver-fraction-a
 frame_lock = threading.Lock()
 latest_jpeg = None
 shutdown_event = threading.Event()
-
-path = BASE_DIR / "recorded_movie.mp4"
-vcodec = "h264"
-MAX_SAVE_FRAME_COUNT = 10000
-fps = 30
-ffmpeg_process = None
-is_recording = False
-stop_requested = False
-f_count = 0
-start_time = 0.0
-
 
 def live_stream_loop():
     while not shutdown_event.is_set():
@@ -43,56 +45,6 @@ def live_stream_loop():
         time.sleep(0.03)
 
 
-
-    if is_recording and ffmpeg_process is None:
-        height, width = frame.shape[:2]
-
-        start_time = time.time()
-        stop_requested = False
-        f_count = 0
-
-
-        ffmpeg_process = (
-            ffmpeg 
-            .input(
-                "pipe:",
-                format="rawvideo",
-                pix_fmt="bgr24", 
-                s=f"{width}*{height}",
-                r=fps
-            )
-            .output(
-                str(path),
-                vcodec=vcodec,
-                pix_fmt="yuv420p",
-                r=fps
-            )
-            .overwrite_output()
-            .run_async(pipe_stdin=True)
-        )   
-        print("録画開始")
-
-    if is_recording and ffmpeg_process is not None:
-        ffmpeg_process.stdin.write(save_frame.tobytes())
-        f_count += 1
-
-    if is_recording and (
-        stop_requested or 
-        f_count >= MAX_SAVE_FRAME_COUNT
-    ):
-        ffmpeg_process.stdin.close()
-        ffmpeg_process.wait()
-        print("録画終了")
-        print("保存先：", path)
-
-        ffmpeg_process = None
-        is_recording = False
-        stop_requested = False
-        f_count = 0
-    time.sleep(0.03)
-
-
-
 @bottle.get('/')
 def index():
     return bottle.template('index')
@@ -105,67 +57,110 @@ def live_stream():
     bottle.response.set_header('Expires', '0')
     return live_stream_loop()
 
+recording_lock  = threading.Lock()
+is_recording = False
+raw_video = None
+
 @bottle.post("/toggle_recording")
 def toggle_recoding():
-    global is_recording
-    global stop_requested
+    global raw_video, is_recording
+
     print("ボタンが押されました")
-    if not is_recording:
-        is_recording = True
-        stop_requested = False
-        return{
-            "recording":True
-        }
-    else:
-        stop_requested = True
-        return{
-            "recording":False
-        }
+
+    with recording_lock:
+        is_recording = not is_recording
+        if is_recording:
+            raw_video = tempfile.NamedTemporaryFile(mode='w+b', delete=False)
+            npy_saver.start_record(raw_video)
+
+            return {
+                "recording": True
+            }
+        else:
+            npy_saver.end_record(raw_video)
+            raw_video.flush()
 
 
-cam = pypuclib.CameraFactory().create()
-decoder = cam.decoder()
-reso = cam.resolution()
+            # 解析処理ここから
 
-GPUStatus = decoder.getAvailableGPUProcess()
+            # raw_video.seek(0)
+            # with open('UI/output/tmp.npy', 'wb') as f:
+            #     shutil.copyfileobj(raw_video, f)
+
+            raw_video.close()
+            # tracking_UImerge.tracking(raw_video.name)
+            print(raw_video.name)
+            # os.remove(raw_video.name)
+
+            # tracking_UImerge.tracking("faster_capture/output/infinicam_coin_toss_meetingroom_10yen_1000fps.npy")
 
 
-if GPUStatus:
-    param = pypuclib.GPUSetup(reso.width, reso.height)
-    decoder.setupGPUDecode(param)
-    print('Decode using a GPU device')
-else:
-    print('Since GPU is not available, decode using CPU')
+            # 解析処理ここまでなはず
 
+
+            
+            
+            return {
+                "recording": False
+            }
+
+decoder = None
+reso = None
+GPUStatus = None
 
 def xfer_callback(xferData):
     global latest_jpeg
     if shutdown_event.is_set():
         return
 
+    with recording_lock:
+        if is_recording:
+            npy_saver.write_frame(raw_video, xferData.data())
+
     if GPUStatus:
         array = decoder.decodeGPU(xferData, True, reso.width)
     else:
         array = decoder.decode(xferData)
+
     success, encoded_image = cv2.imencode('.jpg', array)
     if success:
         with frame_lock:
             latest_jpeg = encoded_image.tobytes()
 
 
-
 def run_server():
     bottle.run(host=HOST, port=PORT, server='waitress')
 
+
+
+FPS = 1000
+WIDTH = 1246
+HEIGHT = 1008
+
 def main():
-    global is_recording
-    global stop_requested
-    global ffmpeg_process
-    global f_count
-    global start_time
+    global decoder, reso, GPUStatus, npy_saver
 
     #ngrok.forward(f'{HOST}:{PORT}', authtoken_from_env=True, domain=PUBLIC_URL)
     threading.Thread(target=run_server, daemon=True).start()
+
+
+    cam = pypuclib.CameraFactory().create()
+    cam.setFramerateShutter(FPS, FPS)
+    cam.setResolution(WIDTH, HEIGHT)
+    decoder = cam.decoder()
+    quantization = decoder.quantization()
+    reso = cam.resolution()
+
+    GPUStatus = decoder.getAvailableGPUProcess()
+    if GPUStatus:
+        param = pypuclib.GPUSetup(reso.width, reso.height)
+        decoder.setupGPUDecode(param)
+        print('Decode using a GPU device')
+    else:
+        print('Since GPU is not available, decode using CPU')
+
+    npy_saver = NpySaver(FPS, WIDTH, HEIGHT, quantization)
+
     try:
         cam.beginXfer(xfer_callback)
         print("INFINICAM開始")
@@ -176,34 +171,21 @@ def main():
         pass
 
     finally:
-        shutdown_event.set()
+        print("プログラムを終了します...")
 
-        if ffmpeg_process is not None:
-            ffmpeg_process.stdin.close()
-            ffmpeg_process.wait()
+        shutdown_event.set()
         cam.endXfer()
 
         if GPUStatus:
             decoder.teardownGPUDecode()
 
+        if raw_video is not None:
+            raw_video.close()
+
         # decoder.teardownGPUDecode()
         print("終了しました")
 
 
-#保存済みの動画をブラウザに渡すためのURL設定
-@bottle.get('/videos/<filename:path>')
-def download_video(filename):
-    res = bottle.static_file(filename, root=str(BASE_DIR), mimetype='video/mp4')
-    
-    res.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
-    res.set_header('Pragma', 'no-cache')
-    res.set_header('Expires', '0')
-    
-    return res
-
 if __name__ == '__main__':
     main()
-    
-    
-
     
